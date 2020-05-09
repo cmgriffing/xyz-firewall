3bf804a6d4d837af23db695c56efcc928bc485bb
Chris Griffing <cmgriffing@gmail.com>
2020-05-09 15:46:34 -0700

    feat: add basic container component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Container from "../components/container"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>Welcome to XYZ Firewall!!</p>
+    <Container>
+      <p>Welcome to XYZ Firewall!!</p>
+    </Container>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
