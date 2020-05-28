c2ebebfa67e9515ec385b95197519c7599bf58f9
Nicholas Ulricksen <nulricksen@yahoo.com>
2020-05-28 00:20:15 -0700

    improvement: add fonts
    re #2

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,12 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import "./globals.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,6 +27,18 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet>
+        {/* Archivo Narrow Font (Body Text) */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+        {/* Archivo Black (Title/Logo) */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
